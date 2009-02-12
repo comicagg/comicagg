73e312e587aedc22a85b25e0820427fab2b8c6c8
Jesús Fernández <esu@kumomi.es>
2009-02-12 17:44:38 +0100

    método unicode para los objetos Task

--- comicagg/todo/models.py
+++ comicagg/todo/models.py
@@ -3,11 +3,14 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-  user = models.ForeignKey(User, default=1)
-  state = models.BooleanField(default=False)
-  title = models.CharField(max_length=255)
-  date = models.DateTimeField(auto_now_add=True)
-  notes = models.TextField()
+	user = models.ForeignKey(User, default=1)
+	state = models.BooleanField(default=False)
+	title = models.CharField(max_length=255)
+	date = models.DateTimeField(auto_now_add=True)
+	notes = models.TextField()
+	
+	class Meta:
+		ordering = ['state']
 
-  class Meta:
-    ordering = ['state']
+	def __unicode__(self):
+		return self.title
