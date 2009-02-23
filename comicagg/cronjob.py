c2867b7f634a097d6abcefb6ab4a6a1a44a816ea
Jesús Fernández <esu@proyectoanonimo.com>
2009-02-23 13:11:06 +0100

    nueva tarea de cron con threads para que sea más rápida

--- comicagg/cronjob.py
+++ comicagg/cronjob.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os, sys
+import os, sys, settings_local, threading, time
 from traceback import *
 from datetime import datetime
-import settings_local
 
 sys.path.insert(0, settings_local.ROOT)
 os.environ['DJANGO_SETTINGS_MODULE'] = "comicagg.settings"
@@ -12,57 +11,89 @@ from comicagg.agregator.models import *
 from django.core.mail import send_mail
 from comicagg.agregator.check import check_comic
 
-print "Hora comienzo: %s" % datetime.now()
 #check all comics
-all = Comic.objects.all()
+all = list(Comic.objects.all())
 new = 0
 no_change = 0
 errors_active = list()
 errors_inactive = list()
-for comic in all:
-  #check for new strip
-  try:
-    h_obj = check_comic(comic)
-  except NoMatchException:
-    s = "  Error comprobando %s" % comic
-    if comic.activo:
-      errors_active.append(s)
-    else:
-      errors_inactive.append(s)
-    continue
-  except KeyboardInterrupt:
-    print "Matado"
-    sys.exit()
-  except:
-    #print_exc()
-    s = "  Error inesperado %s: %s" % (comic.name.encode('utf-8'), sys.exc_info()[1])
-    if comic.activo:
-      errors_active.append(s)
-    else:
-      errors_inactive.append(s)
-    #raise
-    continue
-  if h_obj:
-    new += 1
-    #foreach user who has a subscription, create the unreadcomic object
-    subscriptions = comic.subscription_set.all()
-    for subscription in subscriptions:
-      unread = UnreadComic.objects.get_or_create(user=subscription.user, history=h_obj, comic=subscription.comic)
-    #si es un comic desactivado o terminado y se actualiza notificar posible activacion
-    if not comic.activo or comic.ended:
-      message = 'El desactivado o terminado %s se ha actualizado.\n' % (comic.name,)
-      #send_mail('[CA] Comic desactivado actualizado', message, 'Comic Aggregator <robot@comicagg.com>', ['admin@comicagg.com'])
-  else:
-    no_change += 1
 
-print "Comics activos"
+class CheckThread(threading.Thread):
+	def __init__(self, all, errors_active, errors_inactive):
+		threading.Thread.__init__(self)
+		self.all = all
+		self.errors_active = errors_active
+		self.errors_inactive = errors_inactive
+
+	def run(self):
+		global new
+		global no_change
+
+		comic = self.next()
+		while comic:
+			h_obj = None
+			try:
+				h_obj = check_comic(comic)
+			except KeyboardInterrupt:
+				print '*** Matado %s ***' % (datetime.now())
+				sys.exit()
+			except NoMatchException:
+				s = '   Error comprobando %s\n' % comic.name.encode('utf-8')
+				if comic.activo:
+					self.errors_active.append(s)
+				else:
+					self.errors_inactive.append(s)
+				#continue
+			except:
+				#print_exc()
+				s = '   Error inesperado %s: %s\n' % (comic.name.encode('utf-8'), sys.exc_info()[1])
+				if comic.activo:
+					self.errors_active.append(s)
+				else:
+					self.errors_inactive.append(s)
+				#raise
+				#continue
+			if h_obj:
+				new += 1
+				#foreach user who has a subscription, create the unreadcomic object
+				subscriptions = comic.subscription_set.all()
+				for subscription in subscriptions:
+					unread = UnreadComic.objects.get_or_create(user=subscription.user, history=h_obj, comic=subscription.comic)
+				#si es un comic desactivado o terminado y se actualiza notificar posible activacion
+				if not comic.activo or comic.ended:
+					s = '  El desactivado o terminado %s se ha actualizado.\n' % (comic.name,)
+					self.errors_inactive.append(s)
+			else:
+				no_change += 1
+			comic = self.next()
+
+	def next(self):
+		print "%s: %d left" % (self.getName(), len(self.all))
+		if len(self.all):
+			return self.all.pop(0)
+
+print '\n*** Ejecución de cronjob.py (%s) ***' % datetime.now()
+
+salida = "Hora comienzo: %s\n" % datetime.now()
+thread_list = list()
+for i in xrange(6):
+	t = CheckThread(all, errors_active, errors_inactive)
+	thread_list.append(t)
+	t.start()
+
+for t in thread_list:
+	t.join()
+
+salida += "Comics activos\n"
 for s in errors_active:
-  print s
+  salida += s
 
-print "-------------------------"
-print "Comics desactivados"
+salida += "-------------------------\n"
+salida += "Comics desactivados\n"
 for s in errors_inactive:
-  print s
+  salida += s
 
-print "%s nuevos, %s sin cambios, %s errores" % (new, no_change, (len(errors_active)+len(errors_inactive)))
-print "Hora fin: %s" % datetime.now()
+salida += "%s nuevos, %s sin cambios, %s errores\n" % (new, no_change, (len(errors_active)+len(errors_inactive)))
+salida += "Hora fin: %s\n" % datetime.now()
+
+send_mail('[CA] Salida de la tarea de cron', salida, 'robot@comicagg.com', ['esu@kumomi.es'])
