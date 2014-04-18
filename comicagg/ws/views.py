f9927d9e8462cd6c658a63a0ac61f8db9e77d857
Jesus Fernandez <esu@kumomi.es>
2014-04-18 14:31:59 +0200

    Update SQL query to run with PostgreSQL

--- comicagg/ws/views.py
+++ comicagg/ws/views.py
@@ -18,20 +18,15 @@ def unread_user(request, user):
     sql = """
 SELECT agregator_unreadcomic.comic_id, name, count(agregator_unreadcomic.id) as count
 FROM agregator_unreadcomic
-  INNER JOIN agregator_comic
-    ON agregator_unreadcomic.comic_id=agregator_comic.id
-  INNER JOIN agregator_subscription
-    ON agregator_unreadcomic.comic_id=agregator_subscription.comic_id
-WHERE
-activo=1
-AND
-ended=0
-AND
-agregator_unreadcomic.user_id=%s
-AND
-agregator_subscription.user_id=%s
-GROUP BY agregator_comic.id
+  INNER JOIN agregator_comic ON agregator_unreadcomic.comic_id=agregator_comic.id
+  INNER JOIN agregator_subscription ON agregator_unreadcomic.comic_id=agregator_subscription.comic_id
+WHERE activo=TRUE
+  AND ended=FALSE
+  AND agregator_unreadcomic.user_id=%s
+  AND agregator_subscription.user_id=%s
+GROUP BY agregator_comic.id, agregator_unreadcomic.comic_id, name, agregator_subscription.position
 ORDER BY agregator_subscription.position"""
+
     acursor = connection.cursor()
     acursor.execute(sql, [user.id, user.id])
     rows = acursor.fetchall()
