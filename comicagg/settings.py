eea8d64ad4c3061c683548f9b2e2071bed13af31
Jesus Fernandez <jesus@nublar.net>
2014-05-11 16:52:16 +0200

    Better join of the paths.

--- comicagg/settings.py
+++ comicagg/settings.py
@@ -46,7 +46,7 @@ ROOT = os.path.dirname(os.path.abspath(__file__)) + '/'
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '%smedia' % ROOT
+MEDIA_ROOT = os.path.join(ROOT, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -74,7 +74,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '%stemplates' % ROOT,
+    os.path.join(ROOT, 'templates'),
 )
 
 STATICFILES_DIRS = (
