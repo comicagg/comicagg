d3bd0e27b9036206648201678121aeb5ce479173
Jesús Fernández <jesus@nublar.net>
2016-02-28 20:17:47 +0100

    Remove simple urls and csrf_exempt from several views

--- comicagg/api/urls.py
+++ comicagg/api/urls.py
@@ -1,21 +1,22 @@
 from django.conf.urls import patterns, url
 from django.views.decorators.csrf import csrf_exempt
-from comicagg.api.views import IndexView, ComicsView, StripsView, SubscriptionsView, UnreadsView, UserView
+from comicagg.api.views import (IndexView, ComicsView, StripsView,
+    SubscriptionsView, UnreadsView, UserView)
 
 urlpatterns = patterns('comicagg.api.views',
-    url(r'^$', csrf_exempt(IndexView.as_view()), name='index'),
+    url(r'^$', IndexView.as_view(), name='index'),
 
-    url(r'^comics/simple$', csrf_exempt(ComicsView.as_view()), {'simple':True}, name='comics_simple'),
-    url(r'^comics$', csrf_exempt(ComicsView.as_view()), name='comics'),
-    url(r'^comics/(?P<comic_id>\d+)$', csrf_exempt(ComicsView.as_view()), name='comic_info'),
+    url(r'^comics$', ComicsView.as_view(), name='comics'),
+    url(r'^comics/(?P<comic_id>\d+)$', ComicsView.as_view(), name='comic_info'),
 
-    url(r'^strips/(?P<strip_id>\d+)$', csrf_exempt(StripsView.as_view()), name='strip_info'),
+    url(r'^subscriptions$', csrf_exempt(SubscriptionsView.as_view()),
+        name='subscriptions'),
 
-    url(r'^subscriptions$', csrf_exempt(SubscriptionsView.as_view()), name='subscriptions'),
+    url(r'^unreads$', UnreadsView.as_view(), name='unreads'),
+    url(r'^unreads/(?P<comic_id>\d+)$', csrf_exempt(UnreadsView.as_view()),
+        name='unreads_comic'),
+    url(r'^strips/(?P<strip_id>\d+)$', csrf_exempt(StripsView.as_view()),
+        name='strip_info'),
 
-    url(r'^unreads/simple$', csrf_exempt(UnreadsView.as_view()), name='unreads'),
-    url(r'^unreads$', csrf_exempt(UnreadsView.as_view()), {'with_strips':True}, name='unreads_with_strips'),
-    url(r'^unreads/(?P<comic_id>\d+)$', csrf_exempt(UnreadsView.as_view()), {'with_strips':True}, name='unreads_for_a_comic'),
-
-    url(r'^user$', csrf_exempt(UserView.as_view()), name='user_info'),
+    url(r'^user$', UserView.as_view(), name='user_info'),
 )
