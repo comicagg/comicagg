5cbc1f834251e6367768442e6b4b637aaa4304db
Jesús Fernández <jesus@nublar.net>
2023-12-19 22:21:20 +0100

    Clean up non-app views

--- src/comicagg/common/views.py
+++
@@ -1,7 +0,0 @@
-from django.views.generic.base import TemplateView
-
-class BaseTemplateView(TemplateView):
-    def get_context_data(self, **kwargs):
-        context = super(BaseTemplateView, self).get_context_data(**kwargs)
-        context.update({})
-        return context
