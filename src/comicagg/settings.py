5f51d7be263f76c1aa0ba9e48d8d9db505181dc5
Jesús Fernández <jesus@nublar.net>
2024-01-17 23:17:03 +0100

    Management app added to create generic commands.

--- src/comicagg/settings.py
+++ src/comicagg/settings.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = [
     "django_celery_beat",
     "django_celery_results",
     # Instead of 'django.contrib.admin'
+    "comicagg.management",
     "comicagg.apps.ComicaggAdminConfig",
     "comicagg.accounts",
     # "comicagg.api",
