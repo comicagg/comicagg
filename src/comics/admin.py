256b7339dc124a61ac3a8640e858d82ce28e0f46
Jesús Fernández <jesusfer@users.noreply.github.com>
2024-08-20 18:28:06 +0200

    PR: Fix comics updates (#89)
    Fixes:
    * Fix #71
    
    New:
    * Start update task from comic admin
    * Fail update task on unexpected exceptions
    * Show last update status on admin comic
    * Added admin filters and actions

--- src/comics/admin.py
+++ src/comics/admin.py
@@ -6,6 +6,10 @@ from django.db.models.query import QuerySet
 from django.utils.translation import gettext_lazy as _
 from django.http import HttpRequest
 
+from comics.tasks.update_comics import update_comic_task
+
+from comics.fields import ComicStatus
+
 from .models import Comic, NewComic, Request, Strip, Subscription, Tag, UnreadStrip
 
 # ##############
@@ -22,7 +26,7 @@ class TagInline(admin.TabularInline):
     extra = 0
 
 
-class HasCustomFunction(admin.SimpleListFilter):
+class HasCustomFunctionFilter(admin.SimpleListFilter):
     title = _("custom function")
     parameter_name = "custom"
 
@@ -43,12 +47,55 @@ class HasCustomFunction(admin.SimpleListFilter):
             return queryset.filter(Q(custom_func__isnull=True) | Q(custom_func=""))
 
 
+class LastUpdateFilter(admin.SimpleListFilter):
+    title = _("last update status")
+    parameter_name = "status"
+
+    def lookups(self, request: Any, model_admin: "ComicAdmin") -> list[tuple[Any, str]]:
+        return [
+            ("success", "Success"),
+            ("nomatch", "No match"),
+            ("error", "Error"),
+        ]
+
+    def queryset(
+        self, request: Any, queryset: QuerySet[Comic]
+    ) -> QuerySet[Comic] | None:
+        if self.value() == "success":
+            return queryset.filter(Q(last_update_status="Success"))
+        elif self.value() == "nomatch":
+            return queryset.filter(Q(last_update_status="No match during update"))
+        else:
+            return queryset.filter(Q(last_update_status__startswith="Error:"))
+
+
+@admin.action(description=_("Start update task"))
+def update_comic_via_task(
+    modeladmin: "ComicAdmin", request: HttpRequest, queryset: QuerySet[Comic]
+):
+    for comic in queryset:
+        update_comic_task.apply_async(
+            (comic.id,), periodic_task_name=f"Update comic {comic.id}"
+        )
+
+
+@admin.action(description=_("Mark as active"))
+def mark_as_active(
+    modeladmin: "ComicAdmin", request: HttpRequest, queryset: QuerySet[Comic]
+):
+    queryset.update(status=ComicStatus.ACTIVE)
+
+
+@admin.action(description=_("Mark as broken"))
+def mark_as_broken(
+    modeladmin: "ComicAdmin", request: HttpRequest, queryset: QuerySet[Comic]
+):
+    queryset.update(status=ComicStatus.BROKEN)
+
+
 class ComicAdmin(admin.ModelAdmin):
-    list_display = (
-        "name",
-        "status",
-        "last_update",
-    )
+    actions = [update_comic_via_task, mark_as_active, mark_as_broken]
+    list_display = ("name", "status", "last_update", "last_update_status")
     search_fields = ["name"]
     save_on_top = True
     inlines = [
@@ -110,7 +157,7 @@ class ComicAdmin(admin.ModelAdmin):
             },
         ),
     )
-    list_filter = ["status", "no_images", HasCustomFunction]
+    list_filter = ["status", "no_images", HasCustomFunctionFilter, LastUpdateFilter]
 
 
 class StripAdmin(admin.ModelAdmin):
